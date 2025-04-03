77396cb6723f3514cea1de4d43e15179e0c7d004
Vesta Su-En Tian <vtian@c2r1s3.42singapore.sg>
2025-04-03 13:34:38 +0800

    fixed ft_ultimate_range returning malloc error instead of invalid range

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -3,29 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vtian <vtian@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: vtian <vtian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 15:03:54 by vtian             #+#    #+#             */
-/*   Updated: 2025/03/27 16:04:41 by vtian            ###   ########.fr       */
+/*   Updated: 2025/04/03 13:33:23 by vtian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+// return 0 and range to null if min >= max
+// return -1 and range to null if other error
+// else return size and range from min to max
 int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
 	int	*array;
 
-	i = 0;
-	array = (int *) malloc(sizeof(int) * ((max - min) + 1));
-	if (array == NULL)
-		return (-1);
 	if (min >= max)
 	{
 		array = NULL;
 		return (0);
 	}
+	array = malloc(sizeof(int) * ((max - min) + 1));
+	if (array == NULL)
+		return (-1);
+	i = 0;
 	while (min < max)
 	{
 		array[i] = min;
@@ -37,20 +40,37 @@ int	ft_ultimate_range(int **range, int min, int max)
 }
 
 // #include <stdio.h>
-// int	main(void)
+// int	main(int argc, char **argv)
 // {
-// 	int	min = -10;
-// 	int	max = 10;
-// 	int	i = 0;
 // 	int	*range;
-// 	int	size = ft_ultimate_range(&range, min, max);
-
+// 	int	max;
+// 	int	min;
+// 	int	i;
+// 	int	size;
+// 	if (argc != 3)
+// 	{
+// 		printf("Usage: cmd [0-9] [0-9]\n");
+// 		return (1);
+// 	};
+// 	min = atoi(argv[1]);
+// 	max = atoi(argv[2]);
+// 	size = ft_ultimate_range(&range, min, max);
+// 	if (size == -1)
+// 	{
+// 		printf("Unspecified Error (Length: -1)\n");
+// 		return (1);
+// 	}
+// 	if (size == 0)
+// 	{
+// 		printf("Invalid Range Provided (Length: 0)\n");
+// 		return (1);
+// 	}
 // 	i = 0;
-// 	printf("\nRange Size: %d\n", size);
 // 	while (i < size)
 // 	{
-// 		printf("%d\n", range[i]);
+// 		printf("%d\t", range[i]);
 // 		i++;
 // 	}
-// 	free(range);
+// 	printf("\nLength: %d\n", size);
+// 	return (0);
 // }
